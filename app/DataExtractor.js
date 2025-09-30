dd885278740a57b9fd4a7e7ec789bd3403b3ec3f
semyonnnnn <kanoe.s99@gmail.com>
2025-09-30 16:11:48 +0500

    sorting: almost sorted

--- app/DataExtractor.js
+++ app/DataExtractor.js
@@ -2,21 +2,21 @@
 const { read, utils } = XLSX;
 
 export const DataExtractor = {
+  prop_title: "что мы предлагаем",
+
   async init() {
     this.rawJSON = await this.getRawJSON();
     const res = this.grouped_arrays(this.rawJSON);
 
-    console.log("res:", res);
-
     return this.rawJSON;
   },
-  isDev:
+  is_dev:
     window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1" ||
     (typeof process !== "undefined" && process.env?.NODE_ENV === "development"),
 
   async getRawJSON() {
-    const url = this.isDev
+    const url = this.is_dev
       ? `/media/vacs.csv`
       : `https://66.rosstat.gov.ru/storage/mediabank/vacs.csv`;
     const response = await fetch(url);
@@ -33,15 +33,145 @@ export const DataExtractor = {
           row[i] === null ||
           (typeof row[i] === "string" && row[i].trim() === "")
             ? ""
-            : row[i]
+            : row[i].trim()
         )
-      );
+      )
+      .filter((row) => {
+        return !row.every((i) => !i || i.trim() === "");
+      });
 
     return data;
   },
 
-  grouped_arrays(raw) {
-    return raw;
+  grouped_arrays(raw_data) {
+    const proposal_arr = [];
+    let proposal_last_index;
+
+    for (const [index, row] of raw_data.entries()) {
+      if (row[0].toLowerCase() === "отдел") {
+        proposal_last_index = index;
+        break;
+      }
+      proposal_arr.push(row);
+    }
+
+    const proposal = { content: [] };
+    proposal_arr.forEach((row, i) => {
+      if (row[0].toLowerCase() === this.prop_title) {
+        proposal["title"] = this.prop_title;
+      } else {
+        proposal["content"][i] = row[0];
+      }
+    });
+
+    proposal["content"] = [...proposal["content"]].filter((item) => item);
+
+    const no_proposal_raw = raw_data.slice(
+      proposal_last_index,
+      raw_data.length
+    );
+
+    const by_deps_arr = [];
+    let currentSection = [];
+    for (const row of no_proposal_raw) {
+      if (row[0].toLowerCase() === "отдел") {
+        if (currentSection.length > 0) {
+          by_deps_arr.push(currentSection);
+          currentSection = [];
+        }
+        currentSection.push(row);
+      } else {
+        currentSection.push(row);
+      }
+    }
+
+    if (currentSection.length > 0) {
+      by_deps_arr.push(currentSection);
+    }
+
+    /*
+
+      [
+        {
+          dep, [            
+                vac, {
+                  req: [],
+                  res: [],
+                  con: []
+                },
+                vac, {
+                  req: [],
+                  res: [],
+                  con: []
+                },
+                vac, {
+                  req: [],
+                  res: [],
+                  con: []
+                },
+              ]           
+         },
+         {
+          dep, [            
+                vac, {
+                  req: [],
+                  res: [],
+                  con: []
+                },
+              ]           
+         },
+         {
+          dep, [            
+                {
+                  title: vac, 
+                  content: {
+                    req: [],
+                    res: [],
+                    con: []
+                  },
+                }
+                vac, {
+                  req: [],
+                  res: [],
+                  con: []
+                },
+              ]           
+         },        
+      ]
+
+    */
+
+    const by_deps = [];
+    console.log("by_deps_arr", by_deps_arr);
+
+    by_deps_arr.forEach((dep_arr) => {
+      const department = { vacs: [] };
+      department["dep"] = dep_arr[0][1];
+
+      // const groups = [];
+      let currentGroup = [];
+
+      dep_arr.slice(1, dep_arr.length).forEach((row) => {
+        if (row[0].toLowerCase() === "должность") {
+          if (currentGroup.length > 0) {
+            department["vacs"].push(currentGroup);
+            currentGroup = [];
+          }
+          currentGroup.push(row);
+        } else {
+          currentGroup.push(row);
+        }
+      });
+
+      if (currentGroup.length > 0) {
+        department["vacs"].push(currentGroup);
+      }
+      by_deps.push(department);
+    });
+
+    console.log(by_deps);
+
+    return proposal;
   },
 };
 
