dc75656c2ba13e86dcd35bdcdaf744e3384c7eb0
semyonnnnn <kanoe.s99@gmail.com>
2025-10-30 16:49:43 +0500

    new: styles only

--- app/ApplyStyles.js
+++ app/ApplyStyles.js
@@ -1,26 +1,35 @@
 export const ApplyStyles = {
-  style(config, styles) {
-    if (Array.isArray(config)) {
-      for (const item of config) {
-        this.style(item, styles);
+  init: () => {
+    const table = document.querySelector("table");
+
+    table
+      .querySelectorAll("td")
+      .forEach((td) => (td.innerHTML = td.innerHTML.replace(/&nbsp;/g, "")));
+    table.querySelectorAll("br").forEach((br) => br.remove());
+
+    const vacs = document.querySelectorAll("table:first-of-type td a");
+    vacs.forEach((vac) => vac.classList.add("vac"));
+
+    for (const [index, row] of Object.entries(table.rows)) {
+      const dep_menu = row.querySelector("table");
+      dep_menu.id = "dep_menu_" + index;
+      dep_menu.classList.add("dep_menu");
+      dep_menu.classList.add("hidden");
+
+      for (const cell of row.cells) {
+        const dep = cell.querySelector("p");
+        dep.classList.add("dep_trigger");
+        dep.id = "dep_trigger_" + index;
+
+        const arrow_container = document.createElement("div");
+        arrow_container.classList.add("arrow_container");
+
+        const arrow = document.createElement("div");
+        arrow.classList.add("arrow");
+
+        dep.appendChild(arrow_container);
+        arrow_container.appendChild(arrow);
       }
-      return;
-    }
-    if (config.className) {
-      const elements = document.querySelectorAll(
-        `#${config.id}, .${config.className}`
-      );
-      elements.forEach((dom) => {
-        const classes = config.className.split(/\s+/);
-        classes.forEach((cls) => {
-          if (styles[cls]) {
-            Object.assign(dom.style, styles[cls]);
-          }
-        });
-      });
-    }
-    if (config.children) {
-      this.style(config.children, styles);
     }
   },
 };
